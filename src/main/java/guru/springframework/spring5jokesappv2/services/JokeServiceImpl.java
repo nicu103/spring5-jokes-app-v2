42a24f60e14e910c903786e4cda6035c4d2a899b
Nicolae Taban <nicolae.taban@gmail.com>
2021-12-15 23:49:46 +0200

    Adds Chuck Norris View

--- src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
+++ src/main/java/guru/springframework/spring5jokesappv2/services/JokeServiceImpl.java
@@ -1,6 +1,7 @@
 package guru.springframework.spring5jokesappv2.services;
 
 import guru.springframework.norris.chuck.ChuckNorrisQuotes;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -8,8 +9,8 @@ public class JokeServiceImpl implements JokeService {
 
     private final ChuckNorrisQuotes chuckNorrisQuotes;
 
-    public JokeServiceImpl(ChuckNorrisQuotes chuckNorrisQuotes) {
-        this.chuckNorrisQuotes = chuckNorrisQuotes;
+    public JokeServiceImpl() {
+        this.chuckNorrisQuotes = new ChuckNorrisQuotes();
     }
 
     @Override
